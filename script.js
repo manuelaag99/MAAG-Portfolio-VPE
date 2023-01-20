568f8655c920a825b13000f66682f0c8af0a7867
Manuel Alanis <manuelaag99@gmail.com>
2023-01-20 10:10:51 -0600

    20230120 last commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-document.querySelector("#loader").style.display = "flex"
 document.querySelector("#mainbar").style.display = "none"
+document.querySelector("#loader").style.display = "flex"
 
+// this bit creates a "loader" and fade-in for the website while its resources are being processed 
 window.addEventListener("load", function() {
     function unfade(thing) {
         var op = 0.1;  // initial opacity
@@ -15,16 +16,17 @@ window.addEventListener("load", function() {
         }, 20);
     }
     document.querySelector("#loader").style.display = "none"
-    //document.querySelector("#mainbar").style.display = "block"
     thing = document.querySelector("#mainbar")
     unfade(thing);
 });
 
+// this bit creates variables out of the language buttons and out of the language-labelled sections of the website
 const languageSpanishButton = document.querySelectorAll("#languages h2")[0];
 const languageEnglishButton = document.querySelectorAll("#languages h2")[1];
 const languageSpanishComponents = document.querySelectorAll(".spanish");
 const languageEnglishComponents = document.querySelectorAll(".english");
 
+// this bit makes sure the website changes in response to the language buttons being clicked 
 languageSpanishButton.onclick = function() {
     languageSpanishButton.style.color = "blue";
     for (var i = 0; i < languageEnglishComponents.length; i++) {
